5b42f0b8ceeeafe7c892194055f6d3e1eafb5544
marinqwe <marinmarinbadrov@gmail.com>
2021-02-01 13:12:01 +0100

    validation module update, added crypto to ensure unique uploaded filenames

--- helpers/validation.js
+++ helpers/validation.js
@@ -5,7 +5,7 @@ const emailSchema = Joi.string().required().email();
 const passwordSchema = Joi.string().min(6).required();
 const imageSchema = Joi.any();
 
-function registerValidation(formData) {
+exports.registerValidation = function registerValidation(formData) {
   const schema = Joi.object({
     name: nameSchema,
     email: emailSchema,
@@ -14,41 +14,35 @@ function registerValidation(formData) {
     dateOfBirth: Joi.date().greater('1-1-1900').less('12-31-2020'),
   });
   return schema.validate(formData);
-}
+};
 
-function updateProfileValidation(formData) {
+exports.updateProfileValidation = function updateProfileValidation(formData) {
   const schema = Joi.object({
     name: nameSchema,
     userImg: imageSchema,
   });
   return schema.validate(formData);
-}
+};
 
-function loginValidation(formData) {
+exports.loginValidation = function loginValidation(formData) {
   const schema = Joi.object({
     email: emailSchema,
     password: passwordSchema,
   });
   return schema.validate(formData);
-}
+};
 
-function postValidation(formData) {
+exports.postValidation = function postValidation(formData) {
   const schema = Joi.object({
     title: nameSchema,
     content: Joi.string().required(),
   });
   return schema.validate(formData);
-}
+};
 
-function commentValidation(formData) {
+exports.commentValidation = function commentValidation(formData) {
   const schema = Joi.object({
     content: Joi.string().min(1).required(),
   });
   return schema.validate(formData);
-}
-
-exports.registerValidation = registerValidation;
-exports.updateProfileValidation = updateProfileValidation;
-exports.loginValidation = loginValidation;
-exports.postValidation = postValidation;
-exports.commentValidation = commentValidation;
+};
--- services/uploadImage.js
+++ services/uploadImage.js
@@ -1,16 +1,17 @@
-const multer = require("multer");
+const multer = require('multer');
+const crypto = require('crypto');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads/");
+    cb(null, './uploads/');
   },
   filename: function (req, file, cb) {
-    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+    cb(null, crypto.randomBytes(16).toString('hex') + file.originalname);
   },
 });
 
 const fileFilter = function (req, file, cb) {
-  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+  if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
     cb(null, true);
   } else {
     cb(null, false);
